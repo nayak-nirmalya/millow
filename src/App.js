8c463c6423d3e41617fa65f5ef6fe9e412c23307
Nirmalya Nayak <n.nayak263@gmail.com>
2022-11-23 19:49:01 +0530

    added search bar

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className="cards__section">
         <h3>Welcome to Millow!</h3>
       </div>
