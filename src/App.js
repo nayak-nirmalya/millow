2d4012c3633aec7465fc1abcd1ec3aa2637d0ce1
Nirmalya Nayak <n.nayak263@gmail.com>
2022-11-23 20:10:44 +0530

    setup for reading data from blockchain and ipfs

--- src/App.js
+++ src/App.js
@@ -15,9 +15,39 @@ import config from './config.json'
 
 function App() {
   const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [escrow, setEscrow] = useState(null)
+  const [homes, setHomes] = useState([])
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    const realEstate = new ethers.Contract(
+      config[network.chainId].realEstate.address,
+      RealEstate,
+      provider,
+    )
+    const totalSupply = await realEstate.totalSupply()
+    const homes = []
+
+    for (let i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i)
+      const response = await fetch(uri)
+      const metadata = await response.json()
+      homes.push(metadata)
+    }
+    setHomes(homes)
+    console.log(homes)
+
+    const escrow = new ethers.Contract(
+      config[network.chainId].escrow.address,
+      Escrow,
+      provider,
+    )
+    setEscrow(escrow)
 
     window.ethereum.on('accountsChanged', async () => {
       const accounts = await window.ethereum.request({
