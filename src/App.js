d34f15ef75b775d9a66807acd29e4f97dead9825
Nirmalya Nayak <n.nayak263@gmail.com>
2022-11-23 18:52:57 +0530

    updated navigation cmponent

--- src/App.js
+++ src/App.js
@@ -14,14 +14,18 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json'
 
 function App() {
+  const [account, setAccount] = useState(null)
+
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-    console.log(provider)
 
-    const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
     })
-    console.log(accounts)
   }
 
   useEffect(() => {
@@ -30,6 +34,7 @@ function App() {
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
       <div className="cards__section">
         <h3>Welcome to Millow!</h3>
       </div>
